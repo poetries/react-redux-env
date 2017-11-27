88f05039aff7b82d9e4fd36b2c4b76c90932e4dd
poetries <jingguanliuye@gmail.com>
2017-11-28 00:46:58 +0800

    Update indexPage.js

--- src/containers/indexPage.js
+++ src/containers/indexPage.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react'
+import logo from '../logo.svg';
 
 class componentName extends Component {
     render() {
         return (
             <div>
-                
+                <header className="App-header">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <h2>首页</h2>
             </div>
         );
     }
