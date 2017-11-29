88359964fd2581e775bddf5584b2bb27502e2370
poetries <jingguanliuye@gmail.com>
2017-11-29 17:53:22 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import {Route,BrowserRouter as Router, Switch} from 'react-router-dom'; //v4
 import HomePage from './containers/HomePage';
 import NotFindPage from './containers/NotFindPage';
 
