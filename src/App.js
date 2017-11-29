64b1663ae1fe6f2dd7e9074f62a0e4a3d1ec6661
poetries <jingguanliuye@gmail.com>
2017-11-29 17:22:36 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import HomePage from './containers/HomePage';
+import NotFindPage from './containers/NotFindPage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          {this.props.children}
+           <Switch>
+                {/*相当于IndexRouter*/}
+                <Route exact path='/' component={HomePage} />
+                <Route  component={NotFindPage} />
+            </Switch>
       </div>
     );
   }
--- src/routers/index.js
+++ src/routers/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import {Route,BrowserRouter as Router, Switch} from 'react-router-dom'; //v4
 import App from '../App';
-import indexPage from '../containers/indexPage';
-import NotFindPage from '../containers/NotFindPage';
 
 const routeMap = (
     <Router>
-        <App>
-            <Switch>
-                <Route exact path='/' component={indexPage} />
-                <Route  component={NotFindPage} />
-            </Switch>
-        </App>
+        <App />
     </Router>
 )
 
